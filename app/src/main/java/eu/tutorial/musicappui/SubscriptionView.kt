ab35cbce21fbf7e3fba78fba4878ab37c546f647
Ian <adriangabrielramos010589@gmail.com>
2024-05-04 16:37:25 +0800

    First Commit

---
+++ app/src/main/java/eu/tutorial/musicappui/SubscriptionView.kt
@@ -0,0 +1,55 @@
+package eu.tutorial.musicappui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SubscriptionView(){
+    Column(
+        modifier = Modifier.height(200.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Manage Subscription")
+        Card(
+            modifier = Modifier.padding(8.dp),
+            elevation = 4.dp
+        ) {
+            Column(modifier = Modifier.padding(8.dp)) {
+                Column {
+                    Text(text = "My Musical")
+                    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+                        Text(text = "Free Tier")
+                        TextButton(onClick = { /*TODO*/ }) {
+                            Text(text = "See all Plans")
+                            Icon(imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight, contentDescription = "See all plans")
+                        }
+                    }
+                }
+                Divider(thickness = 1.dp, modifier = Modifier.padding(horizontal = 8.dp))
+                Row(
+                    modifier = Modifier.padding(vertical = 16.dp)
+                ) {
+                    Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Get a Plan")
+                    Text(text = "Get a Plan")
+                }
+            }
+        }
+    }
+}
